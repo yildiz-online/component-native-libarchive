65417b239082b7d5fe71c28ca88da963ca1d8520
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-03 16:12:19 +0100

    [WIP] Support zlib.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cmake ../../c++ \
+-DZLIB_INCLUDE_DIR="${PWD}/zlib/win64/include" \
+-DZLIB_LIBRARY="${PWD}/zlib/win64/lib/libzlibstatic.a" \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
