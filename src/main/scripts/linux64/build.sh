5c3a5f260225796158fe77427c6a4c0c62ce3462
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-28 14:17:02 +0100

    Update build.sh

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,1 +1,13 @@
-return 0
+#!/usr/bin/env bash
+
+cmake ../../c++ \
+-DZLIB_INCLUDE_DIR="${PWD}/zlib/linux64/include" \
+-DZLIB_LIBRARY="${PWD}/zlib/linux64/lib/libzlibstatic.a" \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
+-DZLIB_WINAPI_EXITCODE=0
+
+make install
+r1=$?
+
+exit $r1
